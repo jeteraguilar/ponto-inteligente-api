f4002b3369fb97e8502b9411b54471dabb178e55
Your Namejeteraguilar@gmail.com <you@example.com>
2020-07-11 13:38:16 -0300

    Commit inicial dos arquivos

---
+++ src/test/java/com/jet/pontointeligente/api/PontoInteligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jet.pontointeligente.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoInteligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
